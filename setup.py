854d7c85279e2d9978281f9980a9b8f1d115190e
aniajasiota <aniajasiota@gmail.com>
2024-12-19 08:23:04 +0000

    create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+version = os.getenv(
+    "VERSION", "0.0.0"
+)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
+
+
+
+setup(
+    name="project369367447",
+    version=version,
+    author="xanniaqx",
+    author_email="anna.jasiota@edu.uekat.pl",
+    description="Attendance management system for university",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/aniajasiota/TestyJednostkoweStudentAttendace.git",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": ["TestyJednostkoweStudentAttendance=src.cli.cli:main:main"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
