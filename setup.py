740e9a930aa8fbced1d1e97b8d6488ae56e6f23d
aniajasiota <aniajasiota@gmail.com>
2024-12-23 15:43:24 +0000

    cd try

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 import os
-
 from setuptools import find_packages, setup
 
-with open("requirements.txt") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-version = os.getenv(
-    "VERSION", "0.0.0"
-)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
-
-
+version = os.getenv("VERSION", "0.0.0")  # Fallback to '0.0.0'
 
 setup(
     name="project369367447",
@@ -17,17 +12,16 @@ setup(
     author="xanniaqx",
     author_email="anna.jasiota@edu.uekat.pl",
     description="Attendance management system for university",
-    long_description_content_type="text/markdown",
     url="https://github.com/aniajasiota/TestyJednostkoweStudentAttendace.git",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=requirements,
+    packages=find_packages(), 
+    include_package_data=True,  
+    install_requires=requirements,  
     entry_points={
-        "console_scripts": ["TestyJednostkoweStudentAttendance=main:main"],
+        "console_scripts": ["TestyJednostkoweStudentAttendance=main:main"],  
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.12",
+    python_requires=">=3.12",  
 )
