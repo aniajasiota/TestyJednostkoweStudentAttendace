b1b29d14606b9bc637ff2cede2d23bedd77fdbe3
aniajasiota <aniajasiota@gmail.com>
2024-12-23 16:17:43 +0000

    fixing

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import os
 from setuptools import find_packages, setup
 
-with open("requirements.txt", encoding="utf-8") as f:
-    requirements = f.read().splitlines()
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
 
-version = os.getenv("VERSION", "0.0.0")  # Fallback to '0.0.0'
+version = os.getenv(
+    "VERSION",
+    "0.0.0",  
+)
 
 setup(
     name="project369367447",
@@ -13,15 +16,20 @@ setup(
     author_email="anna.jasiota@edu.uekat.pl",
     description="Attendance management system for university",
     url="https://github.com/aniajasiota/TestyJednostkoweStudentAttendace.git",
-    packages=find_packages(), 
-    include_package_data=True,  
-    install_requires=requirements,  
-    entry_points={
-        "console_scripts": ["TestyJednostkoweStudentAttendance=main:main"],  
-    },
+    packages=find_packages(
+        include=["src.*"],
+        exclude=["tests.*"],
+    ),
+     install_requires=required_packages,  # requirements from requirements.txt
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Intended Audience :: Education",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.12",  
+    python_requires=">=3.13",
+    entry_points={
+        "console_scripts": [
+            "menageStudents=src.main:main",  # script when someone uses menageStudents in console he can run this app
+        ]
+    },
 )
